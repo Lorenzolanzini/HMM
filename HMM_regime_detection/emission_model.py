dd38531d50e94ab7921b1edf2ce40e34e0c82971
Lorenzo <lanzini.lorenzo.08@gmail.com>
2026-06-11 17:35:54 +0200

    Testing Student's distribution

--- HMM_regime_detection/emission_model.py
+++ HMM_regime_detection/emission_model.py
@@ -1,10 +1,24 @@
 import numpy as np
 from abc import ABC, abstractmethod
 from scipy.special import gamma as Gamma
+from scipy.special import gammaln
 from scipy.special import digamma
 from scipy.optimize import brentq
 
 class EmissionModel(ABC):
+
+    '''
+        Parent class of Emission models for the Hmm class
+            
+            - N_hidden : number of hidden states of the model;
+            - data_obs: observation sequences of length T. np.array of shape (N_seq, T) 
+
+        Two parent methods:
+
+            - emission_probability : computes the emission probability of hidden states given emission parameters
+            - update_emission : called in the maximization step of the expectation-maximization algorithm, to update the emission parameters
+
+    '''
 
     def __init__(self, N_hidden, data_obs):
 
@@ -22,8 +36,21 @@ class EmissionModel(ABC):
 
 
 class Discrete_Emission(EmissionModel):
+
+    '''
+
+        Emission model for discrete observations
+           
+    '''
    
     def __init__(self, N_hidden, data_obs, N_obs, B):
+
+        '''
+
+            N_obs : number of discrete outcomes of observations
+            B : emission probability matrix
+
+        '''
 
         super().__init__(N_hidden, data_obs)
         
@@ -59,8 +86,24 @@ class Discrete_Emission(EmissionModel):
         
    
 class Gaussian_Emission(EmissionModel):
+
+
+    '''
+
+        Emission model for discrete observations
+           
+    '''
    
     def __init__(self, N_hidden, data_obs, gauss_params=None):
+
+
+        '''
+
+            Emission model for continuous Gaussian emissions
+
+                - the emission parameters of each hidden states are the mean and the variance
+           
+        '''
 
         super().__init__(N_hidden, data_obs)
         
@@ -83,68 +126,172 @@ class Gaussian_Emission(EmissionModel):
         return Bt
 
     def update_emission(self, gamma, c):
+
+        '''
+            Perform maximization using the closed formula from the log-likelihood derivatives
+
+        '''
          
         self.gauss_params[:, 0] = (gamma[:, :, :] * self.data_obs[:, :, np.newaxis]/c[:, :, np.newaxis]).sum(axis=(0,1)) / (gamma[:, :, :]/c[:, :, np.newaxis]).sum(axis=(0,1))
         self.gauss_params[:, 1] = np.sqrt((gamma[:, :, :] * self.data_obs[:, :, np.newaxis]**2 /c[:, :, np.newaxis]).sum(axis=(0,1)) / (gamma[:, :, :]/c[:, :, np.newaxis]).sum(axis=(0,1)) - self.gauss_params[:, 0]**2)  
 
 
 class Student_Emission(EmissionModel):
-   
-    def __init__(self, N_hidden, data_obs, student_params=None):
 
+    '''
+
+        Emission model for the Student distribution
+
+        Emission parameters of each hidden states are the mean, variance and degree of freedom nu.
+
+           
+    '''
+   
+    def __init__(self, N_hidden, data_obs, student_params=None, count_max = 10, tol_maximization=1e-2, nu_count=1):
+
+        '''
+            count_max : maximum number of inner iterations in the maximization step to find mu, nu and sigma. Default to 10
+            tol_maximization: convergence tolerance in parameters for the inner iterations
+            nu_count : number of iterations for nu without updating mu and sigma
+            converged : (bool) indicates whether parameters update in the inner EM algorithm converged
+        '''
         super().__init__(N_hidden, data_obs)
         
-        self.student_params = np.zeros((N_hidden, 3))
+        self.params = np.zeros((N_hidden, 3))
+        self.count_max = count_max
+        self.tol_maximization = tol_maximization
+        self.nu_count = nu_count
+        self.converged = False
+
         if student_params is None:
-            self.student_params[:, 0] = np.random.rand(N_hidden)       # mu casuali
-            self.student_params[:, 1] = abs(np.random.rand(N_hidden)) + 1 # sigma > 0
-            self.student_params[:, 2] = abs(np.random.rand(N_hidden)) + 3
+            self.params[:, 0] = np.random.rand(N_hidden)       # mu casuali
+            self.params[:, 1] = abs(np.random.rand(N_hidden)) + 1 # sigma > 0
+            self.params[:, 2] = abs(np.random.rand(N_hidden)) + 3
+
         else:
-            self.student_params = student_params
+            self.params = student_params
+        
+        
 
     def Student(self, x, mu, sigma, n):
-
-        return (Gamma((n+1)/2) / (Gamma(n/2)*np.sqrt(n*np.pi)*sigma)) * (1+(1/n)*((x-mu)/sigma)**2)**(-(n+1)/2)
+        # Calculate the log of the PDF components
+        log_pdf = (gammaln((n + 1) / 2) 
+                - gammaln(n / 2) 
+                - 0.5 * np.log(n * np.pi) 
+                - np.log(sigma)
+                - ((n + 1) / 2) * np.log1p((1 / n) * ((x - mu) / sigma)**2))
+        
+        return np.exp(log_pdf)
     
     def MLE_student(self, x, mu, sigma, n):
+
+        '''
+            Derivative of Log likelihood of the Student distribution w.r.t. nu; find its zeros with the brentq method 
+        
+        '''
         z2 = ((x - mu) / sigma) ** 2
         return (0.5*digamma((n+1)/2) - 0.5*digamma(n/2)
                 - 1/(2*n)
                 - 0.5*np.log(1 + z2/n)
-                + z2 * (n+1) * (1 + z2/n)**(-1) / (2 * n**2))
+                +0.5*(n+1)*z2*((1+z2/n)**(-1))/(n**2))
     
-    def sum_MLE(self, n_guess, i, gamma, c):
+    def sum_MLE(self, mu, sigma, n_guess, i, gamma, c):
+        
         scores = self.MLE_student(
             self.data_obs[:, :],        # raw data, not weighted!
-            self.student_params[i, 0],
-            self.student_params[i, 1],
+            mu,
+            sigma,
             n_guess)
+        
         weights = gamma[:, :, i] / c[:, :]   # posterior for state i
+        
         return np.sum(weights * scores)
 
     def emission_probability(self):
         
-        Bt = self.Student(self.data_obs[:, :, np.newaxis], self.student_params[np.newaxis, np.newaxis, :, 0], self.student_params[np.newaxis, np.newaxis, :, 1], self.student_params[np.newaxis, np.newaxis, :, 2])
+        Bt = self.Student(self.data_obs[:, :, np.newaxis], self.params[np.newaxis, np.newaxis, :, 0], self.params[np.newaxis, np.newaxis, :, 1], self.params[np.newaxis, np.newaxis, :, 2])
         
         return Bt
-
+    
     def update_emission(self, gamma, c):
-         
-        self.student_params[:, 0] = (gamma[:, :, :] * self.data_obs[:, :, np.newaxis]/c[:, :, np.newaxis]).sum(axis=(0,1)) / (gamma[:, :, :]/c[:, :, np.newaxis]).sum(axis=(0,1))
-        self.student_params[:, 1] = np.sqrt((gamma[:, :, :] * self.data_obs[:, :, np.newaxis]**2 /c[:, :, np.newaxis]).sum(axis=(0,1)) / (gamma[:, :, :]/c[:, :, np.newaxis]).sum(axis=(0,1)) - self.student_params[:, 0]**2)  
         
-        self.update_nu(gamma, c)
-
-    def update_nu(self,gamma, c, nmin = 3, nmax = 10000):
-
-        for i in range(self.N_hidden):
+        '''
+            The update emission is performed with an inner expectation-maximization algorithm.
             
-            #print(f"state {i}: f(nmin)={self.sum_MLE(nmin, i, gamma, c):.4f}, f(nmax)={self.sum_MLE(nmax, i, gamma, c):.4f}")
-            #self.student_params[i, 2] = brentq(lambda n_guess: self.sum_MLE(n_guess, i, gamma, c), nmin , nmax)
+        '''
+        count = 0
         
-            try:
-                self.student_params[i, 2] = brentq(lambda n_guess: self.sum_MLE(n_guess, i, gamma, c), nmin, nmax)
-            except ValueError:
-                self.student_params[i, 2] = nmax  # cap at nmax, effectively Gaussian
+        while True:
+            weights = self.compute_weights(gamma, c)  # fixed for entire M-step    
+            mu_old = self.params[:, 0].copy()
+            sigma_old = self.params[:, 1].copy()
+            nu_old = self.params[:, 2].copy()
+
+            self.params[:, 0] = (np.sum(weights * self.data_obs[:, :, np.newaxis], axis=(0,1))
+                                / np.sum(weights, axis=(0,1)))
+
+            self.params[:, 1] = np.sqrt(
+                np.sum(weights * (self.data_obs[:, :, np.newaxis] - self.params[np.newaxis, np.newaxis,:, 0])**2, axis=(0,1))
+                / np.sum(weights, axis=(0,1))
+            )
+            #self.params[:, 1] = np.maximum(self.params[:, 1], 1e-6)
+                
+            for i in range(self.N_hidden):
+                self.params[i, 2] = self.safe_solve_nu(self.params[i, 0], self.params[i, 1], i, gamma, c)
+                
+
+             # --- convergence: max relative change across all params and states ---
+            delta_mu    = np.max(np.abs(self.params[:, 0] - mu_old)    / (np.abs(mu_old)    + 1e-8))
+            delta_sigma = np.max(np.abs(self.params[:, 1] - sigma_old) / (np.abs(sigma_old) + 1e-8))
+            
+            delta_nu = np.max(np.exp(-nu_old / 20) * np.abs(self.params[:, 2] - nu_old) / (np.abs(nu_old) + 1e-8)) # empirical value of 20: above 20 we consider the student distribution equivalent to the Gaussian   
+
+            count+=1
+
+            if max(delta_mu, delta_sigma, delta_nu) <= self.tol_maximization: 
+                
+                self.converged = True
+                '''
+                print('#####################################################################################################################################')  
+                print('End Student maximization: achieved accuracy of', self.tol_maximization)
+                '''
+                break
+
+            elif count>self.count_max:
+                self.converged = False
+                '''
+                print('#####################################################################################################################################')  
+                print('End Student maximization: maximum number reached')
+                '''
+                break
+            
+
+    def compute_weights(self, gamma, c):
+
+        weights = np.zeros((self.data_obs.shape[0], self.data_obs.shape[1], self.N_hidden))
+
+        weights[:, :, :] = (gamma[:, :, :]/ c[:, :, np.newaxis]) * (self.params[np.newaxis, np.newaxis, :, 2]+1) / (self.params[np.newaxis, np.newaxis, :, 2] + (self.data_obs[:, :, np.newaxis]-self.params[np.newaxis, np.newaxis, :, 0])**2 / self.params[np.newaxis, np.newaxis, :, 1]**2)
+        
+        return weights
+    
+    def safe_solve_nu(self, mu, sigma, i, gamma, c):
+        # Define the objective function for the root finder
+        f = lambda nu: self.sum_MLE(mu, sigma, nu, i, gamma, c)
+        
+        a, b = 3.001, 600.0
+        fa, fb = f(a), f(b)
+        
+        # Check if signs are the same at boundaries
+        if fa * fb > 0:
+            # If both negative, the root is likely > 600 (Gaussian)
+            
+            if fa<0:
+                return b
+            # If both positive, the root is likely < 3 (Very heavy tails)
+            else:
+                return a
+                
+        return brentq(f, a, b)
+
 
 
--- HMM_regime_detection/hmm.py
+++ HMM_regime_detection/hmm.py
@@ -6,14 +6,22 @@ import scipy as sp
 
 class Hmm:
     
-    def __init__(self, N_hidden, N_obs, emission_model, pi, A=None):
+    def __init__(self, N_hidden, emission_model, pi=None, A=None):
         
+        '''
+            Initialization of the discrete hidden state Markov Model. Parameters:
+
+                - N_hidden : number of hidden states of the model;
+                - emission_model : object EmissionModel as defined in emission_model.py
+                - pi : initial probablities of hidden states
+                - A : transition matrix
+        
+        '''
+
         self.N_hidden = N_hidden
-        self.N_obs = N_obs
         self.Bt = None  ## Emission probabilities of data. Depends on emission_model params
-
         self.emission = emission_model
-
+        
         if A is not None:
 
             self.A = A
@@ -24,6 +32,7 @@ class Hmm:
             self.A = A_rand / A_rand.sum(axis=1, keepdims=True)   # normalize each row
         
         if pi is not None:
+            
             self.pi = pi
         
         else:
@@ -34,52 +43,54 @@ class Hmm:
 
     ''' 
         Methods
-        
     '''
 
     def forward_coeff(self, data_obs):
+
+        '''
+            
+            Scaled forward algorithm to compute forward variables alpha_j(t). 
+            alpha_j(t) are the probabilities of having the set of observations O(t1)O(t2)...O(t), with hidden state h(t) = j at time t, given the emission parameters. 
+
+                - data_obs: observation sequences of length T. np.array of shape (N_seq, T)
+        
+        '''
 
         N_seq = data_obs.shape[0]
         T = data_obs.shape[1]
         
         alpha = np.zeros((N_seq, T, self.A.shape[0]))
-        c = np.zeros((N_seq, T))
+        c = np.zeros((N_seq, T)) # scaling weight
         
-
-        #self.Bt = self.emission.emission_probability()  # return (N_seq, T, N_hidden)
-                
         alpha_0 = self.pi[np.newaxis, :] * self.Bt[:, 0, :]
-        #print(alpha_0.shape)
         
-        c[:, 0] = 1 / (alpha_0.sum(axis=1))
+        c[:, 0] = 1 / (alpha_0.sum(axis=1)+1e-100)
         
         alpha[:, 0, :] = alpha_0[:, :] * c[:, 0, np.newaxis]
         
         
         for t in range(1, T):
-    
-            alpha_t = ((self.A @ (alpha[:, t-1, :].T)).T)*(self.Bt[:, t, :])
             
-            c[:, t] = 1 / (alpha_t.sum(axis = 1))
+            alpha_t = ((self.A.T @ (alpha[:, t-1, :].T)).T) * (self.Bt[:, t, :])
+            
+            c[:, t] = 1 / (alpha_t.sum(axis = 1)+1e-100)
 
             alpha[:, t, :] = alpha_t * c[:, t, np.newaxis]
         
         
         return alpha, c 
 
-    def prob_obs(self, c):
-        
-        logP = -np.sum(np.log(c))
-        
-        return np.exp(logP)
-        
-    def prob_log(self, c):
-        
-        logP = - np.sum(np.log(c)) 
-        
-        return logP
-    
     def backward_coeff(self, data_obs, c):
+
+        '''  
+          
+            Scaled backward algorithm to compute beta_j(t). 
+            beta_j(t) are the probabilities of having the set of observations O(t+1)O(t+2)...O(T) and hidden state h(t)=j at time t, given the emission parameters. 
+
+                - data_obs: observation sequences of length T. np.array of shape (N_seq, T)
+                - c : scaling weights computed in the forward algorithm        
+        
+        '''
 
         N_seq = data_obs.shape[0]
         T = data_obs.shape[1]
@@ -87,9 +98,7 @@ class Hmm:
         beta = np.zeros((N_seq, T, self.A.shape[0]))
 
         beta[:, -1, :] = 1 * c[:, -1, np.newaxis]
-        
-        #Bt = self.emission.emission_probability() 
-        
+         
         for t in range(1, T):
             
             beta_t = (self.A @ ((beta[:, -t, :] * self.Bt[:, -t, :]).T)).T
@@ -98,54 +107,95 @@ class Hmm:
         
         
         return beta
+      
 
-    def update_A_B(self, data_obs):
+    def e_m_algorithm(self, data_obs):
         
+        '''  
+          
+            Iteration of the expectation-maximization (Baum-Welch) algorithm. 
+                
+                - data_obs: observation sequences of length T. np.array of shape (N_seq, T)   
+        
+        '''
+
         N_seq = data_obs.shape[0]
         T = data_obs.shape[1]
          
-        gamma_ij = np.zeros((N_seq, T, self.A.shape[0], self.A.shape[0]))
+        # expectation step
+
+        eps_ij = np.zeros((N_seq, T, self.A.shape[0], self.A.shape[0])) # probability of hidden states h(t) = i and h(t+1) = j, given observation sequence O(1)...O(T) and emission paramenters
         
         alpha, c = self.forward_coeff(data_obs)
         beta = self.backward_coeff(data_obs, c)
 
-        gamma = alpha * beta
+        gamma = alpha * beta # probabilities of each hidden states at all times P(h(t)= i | O,params)  --> equivalent to mixture weights in unsupervised learning for clustering
+        eps_ij[:, :-1, :, :] = alpha[:, :-1, :, np.newaxis] * self.A[np.newaxis, np.newaxis, :, :] * (self.Bt[:, 1:, np.newaxis, :]) * beta[:, 1:, np.newaxis, :]
+
+        likelihood = self.compute_likelihood(data_obs, c)
+
+        ###################################################################################################################################################################################################################
         
-        b = self.Bt[:, 1:, :] # (N_seq, T, N_states)
-        gamma_ij[:, :-1, :, :] = alpha[:, :-1, :, np.newaxis] * self.A[np.newaxis, np.newaxis, :, :] * (b[:, :, np.newaxis, :]) * beta[:, 1:, np.newaxis, :]
+        # maximization step : update transition matrix A and emission parameters, calling the method update_emission of the EmissionModel object
 
-        A_prime = gamma_ij[:, :-1, :, :].sum(axis=(0, 1)) / (((gamma[:, :-1, :]/c[:, :-1, np.newaxis]).sum(axis=(0,1)))[:, np.newaxis])
-
+        A_prime = eps_ij[:, :-1, :, :].sum(axis=(0, 1)) / (((gamma[:, :-1, :]/(c[:, :-1, np.newaxis])).sum(axis=(0,1)))[:, np.newaxis])
         self.emission.update_emission(gamma, c)
         
         pi_prime = gamma[:, 0, :].sum(axis=(0, 1)) / gamma[:, 0, :].sum()
 
-        return A_prime, pi_prime, c
+        return A_prime, pi_prime, c, likelihood
 
-    def Baum_Welch_algorithm(self, data_obs, N_max=100, eps=1e-8):
+    def Baum_Welch(self, data_obs, N_max=100, eps=1e-5):
+
+        '''  
+          
+            Baum-Welch cycle to learn the HMM parameters
+                
+                - data_obs: observation sequences of length T. np.array of shape (N_seq, T)   
+                - N_max : maximum number of iterations
+                - eps : convergence threshold
+
+        '''
 
         step = 0
-        err = 1
-        #_, c = self.forward_coeff(data_obs)
         
-        #P0_AB = self.prob_obs(c)
+        print('############################################################################################################')
+        print('Start learning: Baum-Welch expectation - maximization algorithm ')
+        
         err_list = []
+        err_diff = 1
         
-        while step <= N_max:
+        
+        while step <= N_max and err_diff > eps:
             
             self.Bt = self.emission.emission_probability()  # return (N_seq, T, N_hidden)
-            A_prime, pi_prime, c = self.update_A_B(data_obs)  
+            A_prime, pi_prime, _,likelihood = self.e_m_algorithm(data_obs)  
 
             self.A = A_prime
             self.pi = pi_prime.reshape((-1))
 
-            err_list.append(self.compute_likelihood(data_obs, c))
-  
+            err_list.append(-likelihood)
+            
+            if step >= 2:
+                err_diff = abs((err_list[-1] - err_list[-2])/err_list[-2])
+            
             step +=1
+            
+            print('Iteration #' + str(step) + '------------ -logL = ' + str(err_list[-1]) + r'------------ $|\Delta$| = ' + str(err_diff*100)+'%')
         
+        print('Learning Finished')
+        print('###############################################################################################################')
+
         return err_list
 
     def compute_likelihood(self, obs_seq, c = None):
+
+        '''
+            
+
+            Compute the log-likelihood of the observation sequence obs_seq given the parameters 
+
+        '''
 
         if c is None:
             
@@ -156,6 +206,11 @@ class Hmm:
         return log_likelihood
 
     def Viterbi(self, data_obs):
+
+        '''
+            Implementation of the Viterbi (Decoding) algorithm: it finds the most probaable sequence of hidden state given observations data_obs O(1)...O(T) and model parameters
+
+        '''
 
         v = np.zeros((data_obs.shape[0], data_obs.shape[1], self.N_hidden))
         bt = np.zeros((data_obs.shape[0], data_obs.shape[1], self.N_hidden))
@@ -174,3 +229,16 @@ class Hmm:
         self.decoded_probs = v[:, -1, 0]
         
         self.best_seqs = bt[:, :, 0]
+    
+
+    def prob_obs(self, c):
+        
+        logP = -np.sum(np.log(c))
+        
+        return np.exp(logP)
+        
+    def prob_log(self, c):
+        
+        logP = - np.sum(np.log(c)) 
+        
+        return logP
